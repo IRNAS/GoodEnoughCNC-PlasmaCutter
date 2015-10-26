b4aabebe76bbc3cd351c95a9c261dc8c7b95357d
mihaelm5 <mihaelmisko@hotmail.com>
2015-10-26 10:43:00 +0100

    New Web page

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+jQuery(function($) {'use strict',
+
+	//Header Slider
+	$(function(){
+		$('#headerSlider.carousel').carousel({
+			interval: 5000
+		});
+	});
+
+	//Initialize WOW JS
+	new WOW().init();
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+
+});
--- index.md
+++
@@ -1,6 +0,0 @@
----
-layout: page
-title: Goodenough CNC Plasma Cutter
-tagline: very low cost and simple, suitable for everyone and open
----
-{% include JB/setup %}
